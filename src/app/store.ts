6dd1db7c22f8a3066cf7e53c68f715fcf61c1374
Piotr Wawro <44544007+piotr-wawro@users.noreply.github.com>
2022-05-04 10:51:15 +0200

    Cleanup

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import toolboxReducer from 'components/toolbox/toolboxSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import toolboxReducer from "components/toolbox/toolboxSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     toolbox: toolboxReducer,
   },
 });
